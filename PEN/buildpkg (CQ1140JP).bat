62de6c234bf994973244faf455c7ec1cce716caa
Ryota Nakamura <rn_magi@zeus.eonet.ne.jp>
2012-12-19 18:06:30 +0900

    [Fix]
    Plugin用 のプロジェクトとして penPluginDev を立ち上げたので、
    パッケージ化する際にコピーする classファイル の取得先の変更。

---
+++ PEN/buildpkg (CQ1140JP).bat
@@ -0,0 +1,76 @@
+set PEN_VER=PEN
+set PLUGIN_DIR=%USERPROFILE%\git\PenTestCode\penPluginDev\bin
+
+F not EXIST ％SystemRoot％￥system32￥wbem￥wmic.exe goto WINDOWS_UNKOWN
+WMIC OS GET CAPTION | find "Windows 7" > nul
+IF not errorlevel 1 GOTO WINDOWS_7
+WMIC OS GET CAPTION | find "Windows Vista" > nul
+IF not errorlevel 1 GOTO WINDOWS_VISTA
+WMIC OS GET CAPTION | find "Windows XP" > nul
+IF not errorlevel 1 GOTO WINDOWS_XP
+GOTO WINDOWS_UNKOWN
+
+:WINDOWS_7
+:WINDOWS_VISTA
+set DESKTOP=%USERPROFILE%\Desktop
+goto :process
+:WINDOWS_XP
+set DESKTOP=%USERPROFILE%\デスクトップ
+goto :process
+:WINDOWS_UNKOWN
+goto :eof
+
+:process
+
+set PEN_DIR=%DESKTOP%\%PEN_VER%
+set PEN_WORK=%USERPROFILE%\git\PEN\PEN
+
+mkdir "%PEN_DIR%"
+mkdir "%PEN_DIR%\src"
+
+copy "%PEN_WORK%\*.java" "%PEN_DIR%\src\"
+copy "%PEN_WORK%\IntVParser.jjt" "%PEN_DIR%\src\"
+copy "%PEN_WORK%\Property.ini" "%PEN_DIR%\src\"
+copy "%PEN_WORK%\pen.png" "%PEN_DIR%\src\"
+
+mkdir "%PEN_DIR%\src\Locale"
+copy "%PEN_WORK%\Locale\*.properties" "%PEN_DIR%\src\Locale\"
+
+mkdir "%PEN_DIR%\\src\ButtonList"
+copy "%PEN_WORK%\ButtonList\*.ini" "%PEN_DIR%\src\ButtonList"
+
+cd "%PEN_WORK%\"
+xcopy  /Y /S /EXCLUDE:nocopy.txt "%PEN_WORK%\rossi" "%PEN_DIR%\src\rossi\"
+
+cd "%PEN_WORK%\"
+xcopy  /Y /S /EXCLUDE:nocopy.txt "%PEN_WORK%\org" "%PEN_DIR%\src\org\"
+
+cd "%PEN_WORK%\sample\xDNCL\"
+xcopy  /Y /S /EXCLUDE:nocopy.txt "%PEN_WORK%\sample\xDNCL\sample" "%PEN_DIR%\sample\"
+
+mkdir "%PEN_DIR%\Manual"
+copy "%PEN_WORK%\Manual\*.pdf" "%PEN_DIR%\Manual"
+
+mkdir "%PEN_DIR%\ButtonList"
+copy "%PEN_WORK%\ButtonList\*.ini" "%PEN_DIR%\ButtonList"
+
+mkdir "%PEN_DIR%\lib"
+copy "%PEN_WORK%\lib\*" "%PEN_DIR%\lib"
+
+mkdir "%PEN_DIR%\lib64"
+copy "%PEN_WORK%\lib64\*" "%PEN_DIR%\lib64"
+
+mkdir "%PEN_DIR%\plugin"
+copy "%PLUGIN_DIR%\*.class" "%PEN_DIR%\plugin"
+
+copy "%PEN_WORK%\PEN.url" "%PEN_DIR%\"
+copy "%PEN_WORK%\ChangeLog.txt" "%PEN_DIR%\"
+copy "%PEN_WORK%\ReadMe.txt" "%PEN_DIR%\"
+copy "%PEN_WORK%\*.ini" "%PEN_DIR%\"
+copy "%PEN_WORK%\PEN.bat" "%PEN_DIR%\"
+copy "%PEN_WORK%\PEN.sh" "%PEN_DIR%\"
+copy "%PEN_WORK%\PEN64.sh" "%PEN_DIR%\"
+
+cd "%PEN_WORK%"
+jar cmf META-INF/MANIFEST.MF PEN.jar -C bin .
+move PEN.jar "%PEN_DIR%\"
