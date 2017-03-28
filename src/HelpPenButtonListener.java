2ac4466ce5e76326c8ae657d67b29b395466b4f0
Ryota Nakamura <rn_magi@zeus.eonet.ne.jp>
2017-03-28 14:33:21 +0900

    [Fix] 本家と関係がなくなったので個人リポジトリに移行

--- src/HelpPenButtonListener.java
+++ src/HelpPenButtonListener.java
@@ -18,13 +18,9 @@ public class HelpPenButtonListener implements ActionListener {
 				"に拡張を行った xDNCL言語 を用いています。" + "\n" +
 				"詳しくは [ ./Manual/xDNCL-Language-Manual.pdf ] を参照してください。" + "\n" +
 				"" + "\n" +
-				"連絡先 : <input value=\"pen@s.ogu.ac.jp\" size=\"20\">" + "\n" +
-				"URL   : <input value=\"http://www.media.osaka-cu.ac.jp/PEN/\" size=\"20\">" + "\n" +
 				"" + "\n" +
-				"Copyright(c) 2003-2016" + "\n" +
-				"中村 亮太 : 大阪市立大学 大学院創造都市研究科" + "\n" +
-				"西田 知博 : 大阪学院大学 情報学部" + "\n" +
-				"松浦 敏雄 : 大阪市立大学大学院 創造都市研究科" + "\n" +
+				"Copyright(c) 2003-2017" + "\n" +
+				"中村 亮太" + "\n" +
 				"</pre></html>";
 		
 		info = new MyJLabel(str);
